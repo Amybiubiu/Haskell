c861cc6cb66fa89017bcc41f852bd2ba3ade88b5
Amybiubiu <1246457100@qq.com>
2020-02-25 19:20:13 +0800

    type,type bariables,typeclassed

---
+++ type.hs
@@ -0,0 +1,13 @@
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'z']]
+
+addThree:: Int->Int->Int->Int
+factor:: Integer -> Integer
+read :: (Read a) => String -> a
+
+minBound :: (Bounded a) => a
+
+length :: [a]->Int
+(Num b)=> length :: [a]->b
+
+

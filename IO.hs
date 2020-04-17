dcddf3d1c0c09dc34d8d5b8d4fc215670ea8b70f
Amybiubiu <1246457100@qq.com>
2020-04-17 16:00:41 +0800

    chunk

--- IO.hs
+++ IO.hs
@@ -1,3 +1,10 @@
+import System.IO 
+import System.Directory
+import System.Environment
+import Data.List
+import System.Environment
+import qualified Data.ByteString.Lazy as B
+
 main = do
     line <- getLine
     if null line
@@ -7,3 +14,34 @@ main = do
             main
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
+
+buffer = do
+    withFile "something.txt" ReadMode (\handle -> do
+        hSetBuffering handle $ BlockBuffering (Just 2048)
+        contents <- hGetContents handle
+        putStr contents)
+
+-- 刚刚居然没反应过来dispatch是一个类型？
+dispatch :: [(String,[System]->IO ())]
+dispatch = [ ("add",add)
+           , ("view",view)
+           , ("remove",remove)
+           ]
+
+main = do
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action args
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+-- ByteStrings
+main = do
+    (fileName1:fileName2:_) <- getArgs
+    copyFile' fileName1 fileName2
+
+copyFile' :: FilePath -> FilePath -> IO()
+copyFile' source dest = do
+    contents <- B.readFile source
+    B.writeFile dest contents

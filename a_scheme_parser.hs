1b0fa8dd1cf785861a15c475c4b459d79c8698b2
Amybiubiu <1246457100@qq.com>
2020-07-20 22:50:39 +0800

    init

--- a_scheme_parser.hs
+++ a_scheme_parser.hs
@@ -1,4 +1,32 @@
-import Text.PaserCombinators.Pasec hiding (spaces)
+module Main where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
 
-symbol :: Paser Char
-symbol = oneOF 
+main :: IO ()
+main = do args <- getArgs
+          putStrLn(readExpr (args !! 0))
+
+symbol :: Parser Char   -- what does Parser Char mean
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of 
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+{-- P17 --}
